d9f5a0c809c7cabe7cc07ab83db799e601e9b51d
LetzterMensch <103060332+LetzterMensch@users.noreply.github.com>
2022-10-07 11:49:05 +0700

    Delete demo1 directory

--- demo1/src/main/java/com/example/demo/repository/PhotosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Photo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PhotosRepository extends CrudRepository<Photo, Integer> {
-
-}
