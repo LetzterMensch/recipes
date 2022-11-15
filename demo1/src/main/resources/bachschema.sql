ea3469eb1c0d29566f60e1d097f5a52c1a563f3f
LetzterMensch <103060332+LetzterMensch@users.noreply.github.com>
2022-11-15 10:59:28 +0700

    Delete demo1 directory

--- demo1/src/main/resources/bachschema.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists photos (
-                                      ID bigint primary key AUTO_INCREMENT,
-                                      file_name varchar(255),
-                                      content_type varchar(255),
-                                      data binary
-);
